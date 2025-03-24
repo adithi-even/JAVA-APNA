a7dbd2a10197fd9f727dfa7e1484e42a92bce635
adithi-even <adithigopal1409@gmail.com>
2025-03-24 18:26:27 +0530

    Practiced the variables in the java next is if else in JAVA

--- Main.java
+++ Main.java
@@ -1,13 +1,48 @@
-import java.util.*;
+// import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
+// public class Main {
+//     public static void main(String[] args) {
 
-    Scanner sum = new Scanner(System.in);
+//     Scanner sum = new Scanner(System.in);
           
-    int a = sum.nextInt();
-    int b = sum.nextInt();
-    int sums = a + b;
-    System.out.println(sums);
-    }
-}
+//     int a = sum.nextInt();
+//     int b = sum.nextInt();
+//     int sums = a + b;
+//     System.out.println(sums);
+
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner area = new Scanner(System.in);
+//         int radius = area.nextInt();
+
+//         double circleArea = 3.14 * radius * radius ;
+
+//         System.out.println("the area of the circle is : " + circleArea);
+//     }
+// };
+
+// import java.util.*;
+
+
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner numbers = new Scanner(System.in);
+//         int a = numbers.nextInt();
+//         int b = numbers.nextInt();
+//         int c = numbers.nextInt();
+//         int d = numbers.nextInt();
+//         System.out.println(a);
+//         System.out.println(b);
+//         System.out.println(c);
+//         System.out.println(d);
+
+//     }
+// }
+
+
